77b610bc1dfe211db266ffce11160f3572b0c4ca
Rajesh Raikwar <rajesh.raikwar1211@gmail.com>
2017-05-14 22:41:56 +0530

    minor ui-fixes

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-  var jq = jQuery.noConflict();
+  var $ = jQuery.noConflict();
   var BASE_URL = '';
   var username = '';
   var allFilePatternString = '';
@@ -30,10 +30,10 @@
 
   var deleteIndividualFile = function() {
     setTimeout(function() {
-      // jq('.action_cog').click();
+      // $('.action_cog').click();
       document.getElementsByClassName('ts_icon_cog')[0].click();
-      jq('#delete_file').click();
-      // jq("button:contains('Yes, delete this file')").click()
+      $('#delete_file').click();
+      // $("button:contains('Yes, delete this file')").click()
       if (localStorage.getItem('lastNFilesDeleting') == "1") {
         var remainingFiles = parseInt(localStorage.getItem('numberOfFilesToDelete')) - 1
         localStorage.setItem('numberOfFilesToDelete', remainingFiles);
@@ -62,7 +62,7 @@
     }
 
     allFilePatternStringWithoutUsername = "https:\/\/"+BASE_URL+"\/files"
-    username = jq("#user_menu_name").html();
+    username = $("#user_menu_name").html();
     allFilePatternString = "https:\/\/"+BASE_URL+"\/files\/"+username
     allFilePattern = new RegExp(allFilePatternString);
     lastPagePatternString = "https:\/\/"+BASE_URL+"\/files\/"+username+"\\?page=(.*)"
@@ -78,39 +78,66 @@
   }
 
   var addPluginBlocks = function() {
-    jq('.tab_set').append('<button id="addToDeleteList">Add to delete list</button>');
-    jq('.tab_set').append('<span class="deleteLastSpan"><input type="text" id="deleteLast" placeholder="Delete last n files"/></span>');
-    jq('.tab_set').append('<button id="deleteFiles">Delete</button>');
-    jq('.tab_set').append('<span style="color:red" id="deleteIndicator">Delete in Progress </span>');
+    var deletIndicatorHtml = '<span class="delete-indicator" id="deleteIndicator"> Delete in Progress ... </span>'
+    $("#page_contents").prepend(deletIndicatorHtml);
+    var html = `
+      <div class='sts-action-area'>
+        <div>
+          <button class="btn btn-blue" id="addToDeleteList">Add to delete list</button> 
+          No. of files in the bucket to delete: <span id="filesInBucket">5</span>
+        <div>
+        <div>Or</div>
+        <div>   
+          <input type="number" id="deleteLast" placeholder="Delete last n files"/>
+        </div>
+        <div>
+          <button class="btn" id="deleteFiles">Delete</button>
+        </div>  
+      </div>
+    `
+
+    $('.pagination').after(html)
+    $('#filesInBucket').html(list.length);
 
     observeElement(".file_list_item", function(list) {
       list.each(function() {
-        var file_link = jq(this).find('.title a').attr('href');
-        var html = '<span> <input type="checkbox" class="sts-select" data-link="'+file_link+'"/></span>';
-        jq(this).before(html);
+        var file_link = $(this).find('.title a').attr('href');
+        var html = '<span class="sts-select-span"> <input type="checkbox" class="sts-select" data-link="'+file_link+'"/></span>';
+        $(this).prepend(html);
+        $(".sts-select-span").click(function(e) {
+            e.stopPropagation();
+        });
+
+        $(".sts-select").click(function(e) {
+            e.stopPropagation();
+        });
       })
+    });
+
+    $(".file_list_item").click(function(e) {
+      e.stopPropagation();
     });
   }
 
   var addBindings = function() {
-
     if(localStorage.getItem('deletingInProgress') == "1") {
-      jq("#deleteIndicator").show();
+      $("#deleteIndicator").show();
     }else{
-      jq("#deleteIndicator").hide();
+      $("#deleteIndicator").hide();
     }
-
-    jq("#addToDeleteList").click(function() {
-      jq(".sts-select:checked").each(function(){
-        console.log(jq(this).data)
-        list.push(jq(this).data('link'));
+    
+    $("#addToDeleteList").click(function() {
+      $(".sts-select:checked").each(function(){
+        console.log($(this).data)
+        list.push($(this).data('link'));
       })
       localStorage.setItem('fileDeleteList', JSON.stringify(list));
+      $('#filesInBucket').html(list.length);
     });
 
-    jq("#deleteFiles").click(function() {
-      if(jq("#deleteLast").val() != "") {
-        setLocalStorage('numberOfFilesToDelete', parseInt(jq("#deleteLast").val()));
+    $("#deleteFiles").click(function() {
+      if($("#deleteLast").val() != "") {
+        setLocalStorage('numberOfFilesToDelete', parseInt($("#deleteLast").val()));
         setLocalStorage('lastNFilesDeleting', 1);
       }else{
         setLocalStorage('lastNFilesDeleting', 0);
@@ -127,14 +154,14 @@
           if(localStorage.getItem('lastNFilesDeleting') == 1) {
             if(parseInt(localStorage.getItem('numberOfFilesToDelete')) > 0) {
               if(window.location.href.match(lastPagePattern) != null && 
-                window.location.href.match(lastPagePattern)[1] == jq(".pagination li:nth-last-child(2) a").html()){
+                window.location.href.match(lastPagePattern)[1] == $(".pagination li:nth-last-child(2) a").html()){
                 var goToItem = function(list) {
                   console.log('go now');
                   list.last().click();
                 }  
                 observeElement(".file_list_item", goToItem);
               }else{
-                window.open("/files/"+username+"?page="+jq(".pagination li:nth-last-child(2) a").html() ,'_self')
+                window.open("/files/"+username+"?page="+$(".pagination li:nth-last-child(2) a").html() ,'_self')
                 return;
               }
             }  
@@ -156,7 +183,7 @@
 
   var observeElement = function(element, callback) {
     var observer = new MutationObserver(function (mutations, me) {
-      var list = jq(element);
+      var list = $(element);
       if (list.length != 0) {
         callback(list);
         me.disconnect();
@@ -169,7 +196,7 @@
     });
   }
 
-  jq(document).ready(function() {
+  $(document).ready(function() {
     if (window.location.href.indexOf(BASE_URL+'/files') != -1) {
       init();
       addPluginBlocks();
